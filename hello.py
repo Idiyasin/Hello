d49af6f7ecae9593a9e2a7c46e9e81c3ead1a63a
Idi Duniya <idiyasind@gmail.com>
2023-03-06 15:46:34 -0500

    updated

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
 class Flight():
     def __init__(self, capacity):
-        self.capacity= capa
+        self.capacity= capacity
+        self.passengers = []
+    
+    def add_passenger(self, name):
+        self.passengers.append(name)
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight(3)
+
+people = ["Harry", "ron", "Hermione", "Ginny"]
+for person in people:
+    if flight.add_passenger(person):
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}")
