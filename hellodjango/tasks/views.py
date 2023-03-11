9539c8f75df5afb6e2ccf1d3a62fc9db83eca0c8
Idi Duniya <idiyasind@gmail.com>
2023-03-11 18:23:49 -0500

    why

--- hellodjango/tasks/views.py
+++ hellodjango/tasks/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+tasks = ["hey", "hello", "nice"]
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        
+    })
